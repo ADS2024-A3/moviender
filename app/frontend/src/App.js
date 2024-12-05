e576cce54e5d806c616858bb875b63a8a757bef7
Tom <62304933+tom22-5@users.noreply.github.com>
2024-12-05 17:33:01 +0100

    fixed correct display of stars

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -106,6 +106,9 @@ const App = () => {
             return; // on the last page you can't go to the right
         }
         setCurrentMovies(newCurrentMovies);
+        if (Object.keys(selectedMovies).includes(1)) {
+            console.log("Found match.")
+        }
     } 
 
     const loadRecommendations = async () => {
@@ -206,7 +209,7 @@ const App = () => {
             <Router>
                 <NavBar onColorChange={handleColorChange} onLanguageChange={handleLanguageChange} onSearchChange={handleSearchChange} language={language} /> 
                 <Routes>
-                    <Route exact path="/" element={<SelectMovies user={user} language={language} search={search} onSearchChange={handleMovieSearchChange} currentMovies={currentMovies} genres={genres} currentGenre={currentGenre} onGenreChange={handleGenreChange} handleInteraction={handleInteraction} loadRecommendations={loadRecommendations} loadingMovies={loadingMovies} selectedNumber={Object.keys(selectedMovies).length} handleReset={handleReset} currentMoviePage={currentMoviePage} handlePageChange={handlePageChange} />} />
+                    <Route exact path="/" element={<SelectMovies user={user} language={language} search={search} onSearchChange={handleMovieSearchChange} currentMovies={currentMovies} genres={genres} currentGenre={currentGenre} onGenreChange={handleGenreChange} handleInteraction={handleInteraction} loadRecommendations={loadRecommendations} loadingMovies={loadingMovies} selectedMovies={selectedMovies} handleReset={handleReset} currentMoviePage={currentMoviePage} handlePageChange={handlePageChange} />} />
                     <Route exact path="/recommendations" element={<ShowMovies user={user} language={language} currentMovies={recommendedMovies} loadingMovies={loadingRecommendedMovies} predictions={recommendedMoviesRating} />} />
                     <Route path="*" element={<NoSite language={language} />} />
                 </Routes>
